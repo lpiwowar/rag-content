7244cc8419d27613c516a1d9395d3e5c9f96edee
Lukas Piwowarski <lpiwowar@redhat.com>
2025-03-17 18:31:50 +0200

    Add unittests for asciidoc

---
+++ tests/asciidoc/test_asciidoc_conveter.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestAsciidocConverter(unittest.TestCase):
+
+    def setUp(self):
+        # TODO Implement tests
+        pass
+
+    def test_asciidoc_to_txt_file(self):
+        self.assertTrue(False)
